01032e8a87d12934b8ed210a7399da1496e1d6f4
Logan Wholey <logan.wholey@fireeye.com>
2020-10-09 10:55:46 -0700

    module-7: making gatsby dynamic

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,105 @@
+import path from 'path';
+import fetch from 'isomorphic-fetch';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  // 1. get a template for this page
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  // 2. Query all pizzas
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  // 3. loop over each pizza and create a page for each
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      // what is the url
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
+}
+
+async function turnToppingsIntoPages({ graphql, actions }) {
+  // 1. get a template for this page
+  const toppingTemplate = path.resolve('./src/pages/pizzas.js');
+  // 2. Query all toppings
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+        }
+      }
+    }
+  `);
+  // 3. loop over each topping and create a page for each
+  data.toppings.nodes.forEach((topping) => {
+    actions.createPage({
+      // what is the url
+      path: `topping/${topping.name}`,
+      component: toppingTemplate,
+      context: {
+        topping: topping.name,
+        // TODO Regex for topping
+        toppingRegex: `/${topping.name}/i`,
+      },
+    });
+  });
+}
+
+async function fetchBeersAndTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
+  // 1. fetch a list of beers
+  const res = await fetch('https://sampleapis.com/beers/api/ale');
+  const beers = await res.json();
+  // 2. loop over each one
+  for (const beer of beers) {
+    const nodeMeta = {
+      id: createNodeId(`beer-${beer.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      },
+    };
+    // 3. create a node for that beer
+    actions.createNode({
+      ...beer,
+      ...nodeMeta,
+    });
+  }
+}
+
+export async function sourceNodes(params) {
+  // fetch a list of beers and source them into our gatsby api
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
+}
+
+export async function createPages(params) {
+  // create pages dynamically
+  // 1. Pizzas
+  // 2. Toppings
+  // 3. Slicemasters
+
+  await Promise.all([
+    turnPizzasIntoPages(params),
+    turnToppingsIntoPages(params),
+  ]);
+}
--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -22,7 +22,7 @@ const ToppingsStyles = styled.div`
       padding: 2px 5px;
     }
 
-    .active {
+    &[aria-current='page'] {
       background: var(--yellow);
     }
   }
@@ -57,7 +57,7 @@ function countPizzasInToppings(pizzas) {
   return sortedToppings;
 }
 
-export default function ToppingsFilter() {
+export default function ToppingsFilter({ activeTopping }) {
   // get a list of all the toppings
   // get a list of all the pizzas with their toppings
   const { toppings, pizzas } = useStaticQuery(graphql`
@@ -85,6 +85,10 @@ export default function ToppingsFilter() {
   // link it up...
   return (
     <ToppingsStyles>
+      <Link to="/pizzas">
+        <span className="name">All</span>
+        <span className="count">{pizzas.nodes.length}</span>
+      </Link>
       {toppingsWithCounts.map((topping) => (
         <Link to={`/topping/${topping.name}`} key={topping.id}>
           <span className="name">{topping.name}</span>
